cf38e0123519a5c291e62462f74998a9d6b3469a
TomaszSkrzypek <tomskr3.ts@gmail.com>
2020-06-20 21:05:32 +0200

    Initial commit

---
+++ src/main/java/pl/tomskr/manager_rozanca/ManagerRozancaApplication.java
@@ -0,0 +1,13 @@
+package pl.tomskr.manager_rozanca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerRozancaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagerRozancaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/tomskr/manager_rozanca/ManagerRozancaApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.tomskr.manager_rozanca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerRozancaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
